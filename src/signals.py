064efc11e2b1aca6a14ef1be90ddaca42b2de818
Bailey M.O. <internetuser130@gmail.com>
2026-09-25 12:40:49 +1000

    Update signals.py

--- src/signals.py
+++ src/signals.py
@@ -6,7 +6,7 @@ def signal_metrics(relative_distance): #  , range_rate): #this function calculat
     # todo: add doppler shift to this when the ui is made so the user can set the center frequency for the doppler metics
     c = 299792458 # speed of light in a vacuum
 
-    sig_latency = (relative_distance * 1000) / c # calculates the signals latency
+    sig_latency = (relative_distance * 1000) / c # calculates the signals one way delay OWD
 
     # sig_doppler = frequency * range_rate # calculates the doppler shift a signal
 
